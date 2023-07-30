20f79c5d5f1ea470ec2ace9f313d7c6e2cc5453d
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2023-07-30 13:03:23 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_musobaqa
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_musobaqa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_musobaqa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
